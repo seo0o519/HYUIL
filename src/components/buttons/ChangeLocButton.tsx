5d1166847555a1415c2e294f7e8c0966f6670248
seo0o519 <seoyoung344@naver.com>
2024-09-19 21:59:17 +0900

    feat: 버튼, 슬라이더 구현

---
+++ src/components/buttons/ChangeLocButton.tsx
@@ -0,0 +1,18 @@
+"use client";
+import React, { useState } from "react";
+import gps from "./gps.svg"
+
+
+export default function ChangeLocationButton() {
+  
+  
+  return (
+      <button 
+        className="w-[50px] h-[33.33px] rounded-md bg-walkorcar_blue text-white flex justify-center items-center">
+            <div className="flex flex-col justify-center items-center text-[8px]" >
+            <img className="w-[20px] h-[20px]" src={gps.src}></img>
+            거점 변경
+            </div>
+        </button>
+  );
+}
