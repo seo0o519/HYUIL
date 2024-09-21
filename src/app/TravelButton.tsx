ff455e13f23f2f281e79d803466095154f4413ea
hummingbbird <pop9814@kookmin.ac.kr>
2024-09-21 21:50:43 +0900

    feat #2 - 여행지 검색 버튼 링크로 구현

--- src/app/TravelButton.tsx
+++ src/app/TravelButton.tsx
@@ -1,15 +1,21 @@
 import { ButtonHTMLAttributes } from "react";
 import { FaChevronRight } from "react-icons/fa6";
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>{
-  
+import Link from "next/link";
+
+interface ButtonProps {
+  label?: string;
+  url?: string;
 }
 
-function TravelButton({}:ButtonProps) {
+function TravelButton({ label, url }: ButtonProps) {
   return (
-    <button type="button" className="flex justify-center items-center gap-2 p-3 border border-[#e3e3e3] rounded-full drop-shadow-lg">
-      여행지 검색
+    <Link
+      href={url || ""}
+      className="flex justify-center items-center w-fit px-5 gap-2 p-3  rounded-full shadow-md shadow-[#eaeaea]"
+    >
+      {label}
       <FaChevronRight />
-    </button>
+    </Link>
   );
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
 import TravelButton from "./TravelButton";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
       <p>hello world!!</p>
-      <TravelButton />
     </div>
   );
 }
