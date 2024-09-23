768c744b5e336f71b0918ef0c2d25b13f83fb265
hummingbbird <pop9814@kookmin.ac.kr>
2024-09-23 19:49:59 +0900

    feat #2 - 지도 버튼 컴포넌트 생성

---
+++ src/app/components/buttons/MapChip.tsx
@@ -0,0 +1,19 @@
+
+
+interface MapChipProps {
+  label?: string;
+}
+
+function MapChip({ label }: MapChipProps) {
+  return (
+    <button
+      type="button"
+      className="flex justify-center items-center w-fit px-4 gap-2 py-1 rounded-full shadow-xl focus:border-2 focus:border-[#649EFF]"
+    >
+      {label && label}
+    </button>
+  );
+}
+
+
+export default MapChip;
