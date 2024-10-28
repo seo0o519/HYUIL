5883e19101493e8dd82b8468faab432335849292
seo0o519 <seoyoung344@naver.com>
2024-10-28 13:06:42 +0000

    feat #22 - 세로형 아이템 컨포넌트 구현

---
+++ src/components/sliders/VerticalItem.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import tmp from "../../../public/tmp.jpg";
+
+interface VerticalItemProps {
+  label?: string;
+  imgurl?: string;
+  addr?: string;
+}
+
+function VerticalItem({ label, imgurl, addr, ...props }: VerticalItemProps) {
+  return (
+    <div className="w-[120px] h-fit flex-row">
+      <div className="rounded-[8px] w-[120px] h-[180px] relative overflow-hidden mb-[8px]">
+        <Image
+          alt={"backgroundImg"}
+          layout="fill"
+          src={imgurl ? imgurl : tmp}
+          objectFit="cover"
+        />
+      </div>
+      <span className="text-[14px] block leading-[20px]">{label && label}</span>
+      <span className="text-[12px] text-[#999999] leading-[16px] block">
+        {addr && addr}
+      </span>
+    </div>
+  );
+}
+
+export default VerticalItem;
